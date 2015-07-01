6e5ca97d7f7192fc91de1d5c19e9da4a0128af6b
maorui <maoruiily@163.com>
2015-07-01 17:18:57 +0800

    update

--- doExt_do_DateTimePicker/src/dotest/module/frame/debug/DoPageViewFactory.java
+++ doExt_do_DateTimePicker/src/dotest/module/frame/debug/DoPageViewFactory.java
@@ -8,9 +8,9 @@ public class DoPageViewFactory implements DoIPageViewFactory {
 	private Activity currentActivity;
 	
 	@Override
-	public void closePage(String arg0, String arg1) {
+	public void closePage(String arg0, String arg1, int arg2) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
